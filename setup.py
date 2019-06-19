a61446e337dccd1f3ba5b826ede2a059c0f1828c
Alessandro Di Martino <aledimax@gmail.com>
2019-06-19 13:47:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name='deepbelief',
     version='1.0.0.dev0',
-    packages=find_packages(exclude=['scripts', 'tests']),
+    packages=find_packages(exclude=['examples', 'tests']),
     test_suite="tests",
-    install_requires=['numpy', 'scipy', 'matplotlib', 'h5py', 'pillow']
+    install_requires=[
+        'h5py==2.9.0',
+        'Keras==2.2.4',
+        'matplotlib==3.0.3',
+        'numpy==1.16.4',
+        'Pillow==6.0.0',
+        'scipy==1.1.0',
+        'six==1.12.0',
+        'tensorflow==1.8.0'
+    ]
 )
